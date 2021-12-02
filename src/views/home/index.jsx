6013068957f30d1404cf9d3d5183310b89526da4
rizalyoga <ach.rizal.yogaswara@gmail.com>
2021-12-02 15:48:14 +0700

    Page List Hotel

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -4,6 +4,7 @@ import CardsKota from "../component/cardHome/card-kota.jsx";
 import CardCoba from "../component/cardHome/card-coba.jsx";
 // import NavBarPage from "../component/navbarPage";
 import NavBarHome from "../component/navbarHome";
+import ListHotel from "../pages/list-hotel/list-hotel.jsx";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <CardsKota />
       <CardCoba />
       {/* <CardsHome /> */}
+      {/* <ListHotel /> */}
     </div>
   );
 };
